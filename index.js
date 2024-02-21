7d4082a049e6baf7757b4eab592d89492a1b28a2
Ranbir111 <138311188+Ranbir111@users.noreply.github.com>
2024-02-22 01:41:43 +0545

    updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
 const port = 3000;
 app.use(bodyParser.json());
 
@@ -11,31 +12,34 @@ app.get('/sendmail', (req, res) => {
     const sub = req.body.sub;
     const message = req.body.msg;
 
-    var nodemailer = require('nodemailer');
+    try {
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: emailFrom,
+                pass: password
+            }
+        });
 
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: emailFrom,
-            pass: password
-        }
-    });
+        var mailOptions = {
+            from: emailFrom,
+            to: emailTo,
+            subject: sub,
+            text: message
+        };
 
-    var mailOptions = {
-        from: emailFrom,
-        to: emailTo,
-        subject: sub,
-        text: message
-    };
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+        res.json({ msg: "done" }).status(200);
+    } catch (error) {
+        res.json(error).status(500);
+    }
 
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
-    res.json({msg:"done"}).status(200);
 })
 
 app.listen(port, () => {
